3707e6a7570c343f3d8b9ab4580b385264b3f0c8
Tmalll <40192806+Tmalll@users.noreply.github.com>
2025-11-15 07:34:28 +0800

    update files

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,3 +1,10 @@
+:: 设置代理服务器
+set http_proxy=socks5h://192.168.1.40:10800
+set https_proxy=%http_proxy%
+set HTTP_PROXY=%http_proxy%
+set HTTPS_PROXY=%http_proxy%
+
+
 cd /d %~dp0
 git add .
 git commit -m "update files"
